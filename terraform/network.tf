cfc855c6c3db22c65f205b3c394bf5a90ffa4b61
CA Meijer <hammingweight@gmail.com>
2021-01-03 11:53:11 +0200

    Add comments. Refactoring.

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,23 +1,46 @@
-resource "oci_core_vcn" "test_vcn" {
+# We'll need a virtual cloud network (VCN) which should use a private IP
+# address range like 192.168.0.0/16
+resource "oci_core_vcn" "project_vcn" {
     compartment_id = oci_identity_compartment.project_compartment.id
 
     cidr_block = "192.168.0.0/16"
-    display_name = "test_vcn"
+    display_name = "${var.project_name}_vcn"
 }
 
-resource "oci_core_subnet" "test_subnet" {
+# We'll create a subnet within the VCN for our VMs.
+# We'll also need to specify how traffic is to be routed and
+# security rules for the subnet.
+resource "oci_core_subnet" "project_subnet" {
+    display_name = "${var.project_name}_subnet"
     compartment_id = oci_identity_compartment.project_compartment.id
-
-    cidr_block = "192.168.1.0/24"
-    display_name = "test_subnet"
-    vcn_id = oci_core_vcn.test_vcn.id
-    route_table_id = oci_core_route_table.test_route_table.id
+    cidr_block = "192.168.0.0/24"
+    vcn_id = oci_core_vcn.project_vcn.id
+    route_table_id = oci_core_route_table.project_route_table.id
     security_list_ids = [ oci_core_security_list.test_seclist.id ]
+}
+
+# Define a route table that has one rule that routes all traffic via an
+# internet gateway.
+resource "oci_core_route_table" "project_route_table" {
+    display_name = "${var.project_name}_route_table"
+    compartment_id = oci_identity_compartment.project_compartment.id
+    vcn_id = oci_core_vcn.project_vcn.id
+    route_rules {
+      destination_type = "CIDR_BLOCK"
+      destination = "0.0.0.0/0"
+      network_entity_id = oci_core_internet_gateway.test_internet_gateway.id
+    }
+}
+
+# Create an internet gateway.
+resource "oci_core_internet_gateway" "test_internet_gateway" {
+    compartment_id = oci_identity_compartment.project_compartment.id
+    vcn_id = oci_core_vcn.project_vcn.id
 }
 
 resource "oci_core_security_list" "test_seclist" {
   compartment_id = oci_identity_compartment.project_compartment.id
-  vcn_id = oci_core_vcn.test_vcn.id
+  vcn_id = oci_core_vcn.project_vcn.id
   ingress_security_rules {
     stateless = false
     source = "0.0.0.0/0"
@@ -38,17 +61,3 @@ resource "oci_core_security_list" "test_seclist" {
   }
 }
 
-resource "oci_core_internet_gateway" "test_internet_gateway" {
-    compartment_id = oci_identity_compartment.project_compartment.id
-    vcn_id = oci_core_vcn.test_vcn.id
-}
-
-resource "oci_core_route_table" "test_route_table" {
-    compartment_id = oci_identity_compartment.project_compartment.id
-    vcn_id = oci_core_vcn.test_vcn.id
-    route_rules {
-      destination_type = "CIDR_BLOCK"
-      destination = "0.0.0.0/0"
-      network_entity_id = oci_core_internet_gateway.test_internet_gateway.id
-    }
-}
