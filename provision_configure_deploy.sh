ae68f47867341f68a8bae0f7c9fe81d9dc51c40d
CA Meijer <hammingweight@gmail.com>
2021-01-17 09:48:38 +0200

    Add a helper script to run the playbooks.

---
+++ provision_configure_deploy.sh
@@ -0,0 +1,13 @@
+cd terraform
+terraform apply --auto-approve
+
+cd ../ansible
+source venv/bin/activate
+
+ap() {
+    ansible-playbook -e database_admin_password=$TF_VAR_database_admin_password -e project_name=$TF_VAR_project_name -e webservice_port=$TF_VAR_webservice_port "$@"
+}
+
+ap configure.yml
+ap database_schema.yml
+ap deploy.yml
