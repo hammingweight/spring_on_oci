13a4cdd10a260f9733bc3047c3a705ec69da322f
CA Meijer <hammingweight@gmail.com>
2021-01-28 16:11:51 +0200

    Add some sanity checking.

--- provision_configure_deploy.sh
+++ provision_configure_deploy.sh
@@ -3,10 +3,15 @@ set -e
 
 script_dir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 
+if [ ! -e $script_dir/configuration_parameters_common/tf_vars.sh ]
+then
+    cp $script_dir/configuration_parameters_common/tf_vars.sh.tmpl $script_dir/configuration_parameters_common/tf_vars.sh
+fi
+
 # Provision the infrastructure
-echo "Provisioning servers, a LB and a DB..."
 cd $script_dir/1_provision
 source $script_dir/configuration_parameters_common/tf_vars.sh
+echo "Provisioning servers, a LB and a DB..."
 terraform init
 terraform destroy --auto-approve
 terraform apply --auto-approve
