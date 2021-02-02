a47ced298452ee1cce7b70c4a04360cd6ae6ebbc
CA Meijer <hammingweight@gmail.com>
2021-02-02 07:07:19 +0200

    Add code comments.

--- 0_spring_application/src/main/java/com/example/demo/controllers/HelloController.java
+++ 0_spring_application/src/main/java/com/example/demo/controllers/HelloController.java
@@ -22,6 +22,7 @@ import java.util.Map;
 @RequestMapping(path = "/hello", produces = "application/json")
 public class HelloController {
 
+    // We use a JDBC Template to query the database.
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
@@ -30,19 +31,23 @@ public class HelloController {
         return sayHello("world");
     }
 
+    // This method needs to be transactional since we query a row in the database in one call and
+    // then update it in a subsequent call so we need to lock the row in a transaction.
     @Transactional
     @GetMapping("/{name}")
     public Map<String, Object> sayHello(@PathVariable("name") String name) {
-        // SELECT ... FOR UPDATE so that we can lock the row to prevent concurrent modifications.
+        // SELECT ... FOR UPDATE so that we lock the row to prevent concurrent modifications.
         String sqlQuery = "SELECT num_visits FROM visitors WHERE name=? FOR UPDATE";
         List<BigDecimal>  numVisits = jdbcTemplate.query(sqlQuery, new SingleColumnRowMapper<>(), name);
         Map<String, Object> map = new HashMap<>();
         if (numVisits.size() == 0) {
+	    // If we got no results, then the name doesn't exist in the DB and we should insert it.
             String sqlInsert = "INSERT INTO visitors (name, num_visits) VALUES (?, 1)";
             jdbcTemplate.update(sqlInsert, name);
             map.put("message", "Hello, " + name);
             map.put("visits", 1);
         } else {
+	    // The name exists. Increment the number of visits to the endpoint.
             BigDecimal count = numVisits.get(0).add(BigDecimal.ONE);
             String sqlUpdate = "UPDATE visitors SET num_visits=? WHERE name=?";
             jdbcTemplate.update(sqlUpdate, count, name);
