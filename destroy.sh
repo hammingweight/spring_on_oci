501ab612ac7e7699d5df75e62176c6600c309f2c
CA Meijer <hammingweight@gmail.com>
2021-02-03 07:42:44 +0200

    Ensure that the tf_vars.sh script exists before trying to tear down the infrastructure.

--- destroy.sh
+++ destroy.sh
@@ -2,6 +2,12 @@
 set -e
 
 script_dir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+
+if [ ! -e $script_dir/configuration_parameters_common/tf_vars.sh ]
+then
+    cp $script_dir/configuration_parameters_common/tf_vars.sh.tmpl $script_dir/configuration_parameters_common/tf_vars.sh
+fi
+
 cd $script_dir/1_provision
 source $script_dir/configuration_parameters_common/tf_vars.sh
 terraform destroy
