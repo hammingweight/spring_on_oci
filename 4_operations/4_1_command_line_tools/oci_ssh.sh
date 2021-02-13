bb0f0c9b69bb171edf8977cfcd85e19da7647045
CA Meijer <hammingweight@gmail.com>
2021-02-13 10:21:20 +0200

    Document the oci_ssh script.

--- 4_operations/4_1_command_line_tools/oci_ssh.sh
+++ 4_operations/4_1_command_line_tools/oci_ssh.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+# Running "ssh -i vm_ssh_key opc@ip_address." This script requires only
+# the ip address and locates the key and adds the user.
 if [ $# != 1 ];
 then
     echo "Usage: $0 <ip_address>"
@@ -8,5 +10,6 @@ then
 fi
 
 script_dir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+# Get the key location from the environment variables file.
 source $script_dir/../../configuration_parameters_common/tf_vars.sh 1>/dev/null 2>/dev/null
 ssh -o  StrictHostKeyChecking=no -i $TF_VAR_vm_ssh_private_key opc@$1
