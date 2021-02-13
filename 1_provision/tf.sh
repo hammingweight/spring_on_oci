2849948b1c5d8593adcd2e180f369fac3d0f1a13
CA Meijer <hammingweight@gmail.com>
2021-02-13 10:01:50 +0200

    Comment tf.sh.

--- 1_provision/tf.sh
+++ 1_provision/tf.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+# A very simple wrapper around terraform that populates the
+# environment vraiables before invoking terraform.
 script_dir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 source $script_dir/../configuration_parameters_common/tf_vars.sh
 terraform $@
