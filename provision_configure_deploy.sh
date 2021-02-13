5f1c2aa2b38f3756a6eb509d4235139f313871a6
CA Meijer <hammingweight@gmail.com>
2021-02-13 10:26:05 +0200

    Comment the provisioning script.

--- provision_configure_deploy.sh
+++ provision_configure_deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# A script that provisions resources in OCI for a Spring
+# boot application and then configures the servers and
+# deployes the application to the servers.
+
 # Check that the user has installed the OCI CLI
 if ! which oci
 then
@@ -19,6 +23,7 @@ fi
 # Get the path to this script
 script_dir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 
+# If the user hasn't created their own tf_vars.sh script, use the template.
 if [ ! -e $script_dir/configuration_parameters_common/tf_vars.sh ]
 then
     cp $script_dir/configuration_parameters_common/tf_vars.sh.tmpl $script_dir/configuration_parameters_common/tf_vars.sh
@@ -35,7 +40,7 @@ terraform apply --auto-approve
 echo "Configuring the servers..."
 $script_dir/ansible_common/ap.sh $script_dir/2_configure/configure.yml
 
-# Deploy the application to the servers
+# Create the database schema and deploy the application to the servers
 echo "Creating database schema..."
 $script_dir/ansible_common/ap.sh $script_dir/3_deploy/schema.yml
 echo "Deploying Spring Boot service to the servers..."
