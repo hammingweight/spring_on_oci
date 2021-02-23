d71c12061e38c599ef325c6627d058c1a1523d49
CA Meijer <hammingweight@gmail.com>
2021-02-23 21:24:44 +0200

    Add a JavaDoc comment.

--- 0_spring_application/src/main/java/com/example/demo/models/VisitorGreeting.java
+++ 0_spring_application/src/main/java/com/example/demo/models/VisitorGreeting.java
@@ -3,11 +3,18 @@ package com.example.demo.models;
 import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+/**
+ * A model of a greeting for a visitor to a REST
+ * endpoint. The object contains a message and an
+ * invocation count.
+ */
 @JsonPropertyOrder({ "visits", "message" })
 public class VisitorGreeting {
 
+    // A message to display.
     private String message;
 
+    // How many times has an endpoint been invoked.
     private int numberOfVisits;
 
     public void setMessage(String message) {
